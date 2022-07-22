6e6abf228ea1b48f4a59595b65948db06bb26a55
Sardar Sultanaliev <sultanaliev_s@auca.kg>
2022-07-22 14:37:39 +0600

    Change logic of updating the picture

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,23 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
   import { onMount } from "svelte";
   import { getPicture } from "./api";
-  import Image from "./Image.svelte";
+  import Image from "./components/Image.svelte";
 
   let picture = {};
-  let isLoading = true;
 
   async function update() {
-    isLoading = true;
+    console.log(2);
     const res = await getPicture();
     picture = res[0];
-    isLoading = false;
   }
+
   onMount(async () => {
     update();
   });
 </script>
 
 <main class="w-screen h-screen flex flex-col justify-center items-center">
-  {#if !isLoading}
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <Image src={picture.url} />
-  {:else}
-    <div class="h-[500px] flex items-center">
-      <img
-        src="https://c.tenor.com/On7kvXhzml4AAAAi/loading-gif.gif"
-        alt="Loading..."
-      />
-    </div>
-  {/if}
+  <Image src={picture.url} />
   <button
     on:click={update}
     class="m-4 p-4 bg-red-500 hover:bg-red-600 rounded-md text-white"
---
+++ src/components/Image.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let src;
+
+    let loading = false;
+    const img = new Image();
+
+    $: {
+        img.src = src;
+        loading = true;
+
+        img.onload = () => {
+            loading = false;
+        };
+    }
+</script>
+
+{#if !loading}
+    <img {src} alt="A cat" class="" />
+{:else}
+    <div class="h-[500px] flex items-center">
+        <img
+            src="https://c.tenor.com/On7kvXhzml4AAAAi/loading-gif.gif"
+            alt="Loading..."
+        />
+    </div>
+{/if}
